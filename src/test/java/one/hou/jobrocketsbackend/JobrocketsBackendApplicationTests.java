0177d62e2250096da4638b6d2d0a3545ad3ee29d
Scott <99236100+scotth82@users.noreply.github.com>
2023-11-28 23:41:00 -0600

    initial setup

---
+++ src/test/java/one/hou/jobrocketsbackend/JobrocketsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package one.hou.jobrocketsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobrocketsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
